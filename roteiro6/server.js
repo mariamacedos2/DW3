bb845c0ec0153339538abf1e72b5dd89f2017a07
Maria Eduarda <163424987+mariamacedos2@users.noreply.github.com>
2026-05-19 22:18:06 -0300

    ..

---
+++ roteiro6/server.js
@@ -0,0 +1,51 @@
+// @file: server.js
+
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+import tarefaRoutes from './src/routes/tarefa.routes.js'
+
+import TarefaRepository from './src/repositories/tarefa.repository.js'
+import TarefaService from './src/services/tarefa.service.js'
+import TarefaController from './src/controllers/tarefa.controller.js'
+
+const server = Fastify()
+
+server.register(cors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS']
+})
+
+// CRIA AS DEPENDÊNCIAS
+
+const repository =
+  new TarefaRepository()
+
+const service =
+  new TarefaService(repository)
+
+const controller =
+  new TarefaController(service)
+
+// REGISTRA AS ROTAS
+
+server.register(tarefaRoutes, {
+  controller
+})
+
+const PORT = 3000
+
+server.listen(
+  { port: PORT },
+  (err) => {
+
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    console.log(
+      `Servidor rodando em http://localhost:${PORT}`
+    )
+  }
+)
